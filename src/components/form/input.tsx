d96c286f7717de5ddc2af1770a84086153a4639e
Cédrick <33513516+cle-guen@users.noreply.github.com>
2024-09-27 14:30:34 +0200

    C/get suggested profiles (#9)
    * suggested
    
    * suggested
    
    * suggested
    
    * suggested
    
    * suggested
    
    * create profil error success

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -3,16 +3,24 @@ interface Props {
   type?: string
   placeholder?: string
   value?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export function Input({ name, placeholder, type = 'text', value }: Props) {
+export function Input({
+  name,
+  placeholder,
+  type = 'text',
+  value,
+  onChange,
+}: Props) {
   return (
     <input
       type={type}
       value={value}
-      className="bg-transparent border border-foreground p-2"
+      className="bg-transparent border border-foreground p-2 w-full"
       placeholder={placeholder}
       name={name}
+      onChange={onChange}
     />
   )
 }
--- src/components/profile/create-profile.tsx
+++ src/components/profile/create-profile.tsx
@@ -1,14 +1,32 @@
 'use client'
 
-import { createProfileAction } from '@/actions/create-profile-action'
-import { useCurrentWallet } from '@/components/auth/hooks/useCurrentWallet'
-import { Card } from '../common/card'
-import { Input } from '../form/input'
-import { SubmitButton } from '../form/submit-button'
+import { useCurrentWallet } from '@/components/auth/hooks/use-current-wallet'
+import { Alert } from '@/components/common/alert'
+import { Card } from '@/components/common/card'
+import { Input } from '@/components/form/input'
+import { SubmitButton } from '@/components/form/submit-button'
+import { useCreateProfile } from '@/components/profile/hooks/use-create-profile'
+import { useState } from 'react'
 
 export function CreateProfile() {
   const { walletAddress, mainUsername, loadingMainUsername } =
     useCurrentWallet()
+  const [username, setUsername] = useState('')
+  const { createProfile, loading, error, response } = useCreateProfile()
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (walletAddress && username) {
+      await createProfile(username, walletAddress)
+    }
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+
+    const validValue = value.toLowerCase().replace(/[^a-z0-9]/g, '')
+    setUsername(validValue)
+  }
 
   if (loadingMainUsername) {
     return null
@@ -20,16 +38,24 @@ export function CreateProfile() {
         <div className="w-1/2">
           <h2 className="text-xl mb-3">Create Profile</h2>
           <Card>
-            <form action={createProfileAction} className="flex flex-col gap-3">
-              <Input name="username" placeholder="Username" />
-              <Input
-                type="hidden"
-                name="ownerWalletAddress"
-                value={walletAddress}
-              />
-              <SubmitButton>Create Profile</SubmitButton>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+              <div className="flex items-center">
+                <Input
+                  value={username}
+                  onChange={handleInputChange}
+                  name="username"
+                  placeholder="Username"
+                />
+              </div>
+              <SubmitButton>
+                {loading ? 'Creating...' : 'Create Profile'}
+              </SubmitButton>
             </form>
           </Card>
+          {error && <Alert type="error" message={error} />}
+          {response && (
+            <Alert type="success" message="Profile created successfully!" />
+          )}
         </div>
       )}
     </>
