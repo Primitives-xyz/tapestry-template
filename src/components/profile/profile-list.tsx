63617c1fa806c37698c8cce4540dcd56e643bbd7
Marcus Smith <marcushsmith@comcast.net>
2024-09-27 07:24:02 -0700

    update profile all endpoint

--- src/components/profile/profile-list.tsx
+++ src/components/profile/profile-list.tsx
@@ -4,16 +4,15 @@ import { getProfilesList } from '@/lib/tapestry'
 export async function ProfilesList() {
   const data = await getProfilesList({})
 
-  const profiles = data.nodes.filter((node: any) => {
-    return node.labels.includes('profile')
-  })
+  const profiles = data.profiles
 
+  console.log('profile', profiles)
   return (
     <div>
       {profiles.map((profile: any, index: number) => {
         return (
           <div className="mb-4" key={index}>
-            <Profile username={profile.properties.username} />
+            <Profile username={profile.profile.username} />
           </div>
         )
       })}
